be78e08c947cb01a2f156bb7fa018a5615bb04e3
marta-santos <109400186+MartaSSantos@users.noreply.github.com>
2023-10-10 14:31:00 -0300

    Mudei a forma de avisar na tela que foi copiada a menssagem

--- script.js
+++ script.js
@@ -5,35 +5,45 @@ let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
+let copiado = document.querySelector("#copiado");
+console.log(copiado.innerHTML)
 
 let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@%$*&';
 let novaSenha = '';
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function() {
-  sizePassword.innerHTML = this.value;
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value;
 }
 
 // function to generate password with all characters
 // função para gerar senha com todos os caracteres
 
-function generatePassword(){
+function generatePassword() {
 
-  let pass = '';
-  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-    pass += charset.charAt(Math.floor(Math.random() * n));
-  }
-  
-  console.log(pass)
-  containerPassword.classList.remove("hide");
-  password.innerHTML = pass;
-  novaSenha = pass;
+    let pass = '';
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
 }
 
 // Function to display a message that the password has been successfully copied
 // Função para exibir uma mensagem informando que a senha foi copiada com sucesso
-function copyPassword(){
-    alert(" SENHA COPIADA COM SUCESSO! 😀🎉")
-  navigator.clipboard.writeText(novaSenha);
+function copyPassword() {
+    copiado.innerHTML = " SENHA COPIADA COM SUCESSO! 😀🎉";
+
+    const intervelAD = setTimeout(myCallback, 5000);
+
+    navigator.clipboard.writeText(novaSenha);
+}
+
+function myCallback() {
+    copiado.innerHTML = "";
+
 }
